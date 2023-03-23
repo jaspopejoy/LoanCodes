8e50d1afce286323afcc853db0db79ef4437d672
jaspopejoy <120056960+jaspopejoy@users.noreply.github.com>
2023-03-23 16:24:43 -0500

    final commit

--- js/site.js
+++ js/site.js
@@ -2,100 +2,101 @@
 //get the values from the interface
 //start or controller function
 function getValues() {
-    //get user values from document
-    let loanAmt = document.getElementById("loanAmt").value;
-    let termMonths = document.getElementById("termMonths").value;
+    //get user values from the document
+    let loanAmount = document.getElementById("loanAmt").value;
+    let numberOfPayments = document.getElementById("numPayments").value;
     let interestRate = document.getElementById("interestRate").value;
 
-    //parse for numbers
-    loanAmt = parseInt(loanAmt);
-    termMonths = parseInt(termMonths);
+    //parse numbers
+    loanAmount = parseInt(loanAmount);
+    numberOfPayments = parseInt(numberOfPayments);
     interestRate = parseInt(interestRate);
 
     //check to make sure they are integers
-    if (Number.isInteger(loanAmt) && Number.isInteger(termMonths) && Number.isInteger(interestRate)) {
+    if (Number.isInteger(loanAmount) && Number.isInteger(numberOfPayments) && Number.isInteger(interestRate)) {
 
-        //call calculateMortgage
-        let loanObj = calculateMortgage(loanAmt, termMonths, interestRate);
+        //call calculateValues
+        let loanArray = calculateValues(loanAmount, numberOfPayments, interestRate);
 
-        //call displayData and write values to the screen
-        displayloanData(loanObj);
-
-        //call displayTable
-        displayTable(loanObj);
+        //call displayResults
+        displayResults(loanArray);
 
     } else {
-        alert("You must enter an Integer!")
+        alert("You must enter valid numbers");
     }
-
 }
 
 //logic function(s)
 //calculate the loan amt, loan term, and interest rate
-function calculateMortgage(loanAmt, termMonths, interestRate) {
+function calculateValues(loanAmount, numberOfPayments, interestRate) {
 
-    let loanObj = {};
+    //assign variables and set array
+    let returnArray = [];
+    let remainingBalance = loanAmount;
+    let totalInterestPaid = 0;
 
-    //calculate monthly payment
-    let monthlyPymt = ( (loanAmt) * (interestRate / 1200) ) / (1 - (1 + interestRate / 1200) ** (-termMonths));
+    //loop through the values and push to returnArray
+    for (let i = 1; i <= numberOfPayments; i++) {
+        returnArray.push(i);
 
+        //calculate monthly payments
+        let monthlyPayment = (loanAmount * ((interestRate / 1200) / (1 - Math.pow(1 + (interestRate / 1200), (numberOfPayments * -1)))));
+        returnArray.push(monthlyPayment);
+
+        //calculate interest paid and principal paid and push into array
+        let interestPaid = remainingBalance * (interestRate / 1200);
+        let principalPaid = monthlyPayment - interestPaid
+        returnArray.push(principalPaid);
+        returnArray.push(interestPaid);
+
+        //calculate total interest paid and push to array
+        totalInterestPaid += interestPaid;
+        returnArray.push(totalInterestPaid);
+
+        //calculate balance and push to array
+        remainingBalance -= principalPaid;
+        returnArray.push(remainingBalance);
+    }
+
+    //print monthly payment to the display
+    document.getElementById("monthlyPayment").classList.add("output-payment");
+    document.getElementById("monthlyPayment").innerHTML = `${returnArray[1].toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })}`;
+
+    //print total principal to the display
+    document.getElementById("totalPrincipal").innerHTML = `${loanAmount.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })}`;
+
+    //print total interest to the display
+    document.getElementById("totalInterest").innerHTML = `${totalInterestPaid.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    })}`;
+    
     //calculate total cost
-    let totalCost = (monthlyPymt * termMonths);
+    let totalCost = totalInterestPaid + loanAmount;
 
-    //calculate total Interest
-    let totalInterest = (totalCost - loanAmt);
-
-    //create loan object to hold loan
-    loanObj.loanAmt = loanAmt;
-    loanObj.termMonths = termMonths;
-    loanObj.interestRate = interestRate;
-    loanObj.monthlyPymt = monthlyPymt;
-    loanObj.totalCost = totalCost;
-    loanObj.totalInterest = totalInterest;
-
-    return loanObj;
-}
-
-//display total principal, monthly payment, total interest, & total cost
-function displayloanData(loanObj) {
-
-    //display total principal to the screen
-    document.getElementById("totalPrincipal").innerHTML = loanObj.loanAmt.toLocaleString('en-US', {
+    //print total cost to the screen
+    document.getElementById("totalCost").innerHTML = `${totalCost.toLocaleString('en-US', {
         style: 'currency',
         currency: 'USD',
         minimumFractionDigits: 2
-    });
+    })}`;
 
-    //display monthly payment    
-    document.getElementById("monthlyPayment").innerHTML = loanObj.monthlyPymt.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    });
-
-    //display total cost
-    document.getElementById("totalCost").innerHTML = loanObj.totalCost.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    });
-
-    //display total interest
-    document.getElementById("totalInterest").innerHTML = loanObj.totalInterest.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    });
-
+    return returnArray;
 }
 
 //display amortization table
-function displayTable(loanObj) {
+function displayResults(loanArray) {
 
-    //assign variables
-    let monthlyInterest=0;
-
-    //get the table body element from the page
+    //get the tale body element from the page
     let tableBody = document.getElementById("results");
 
     //get the template row
@@ -104,58 +105,45 @@ function displayTable(loanObj) {
     //clear the table first
     tableBody.innerHTML = "";
 
-    // loop through the values and add to creaed table
-    for (let i = 1; i <= loanObj.termMonths; i++) {
+    // loop through and add values to the created table
+    for (let index = 0; index < loanArray.length; index += 6) {
 
-        let tableRow = document.importNode(templateRow.content,true);
+        let tableRow = document.importNode(templateRow.content, true);
 
-        let interestPymt = loanObj.loanAmt * (loanObj.interestRate/1200);
-        monthlyInterest += interestPymt;
-        let monthlyPrincipal = loanObj.monthlyPymt - interestPymt;
-        let balance = loanObj.loanAmt - monthlyPrincipal;
-        
-        //grab the tds to put into array
+        //grabs the tds to put into array
         let rowCols = tableRow.querySelectorAll("td");
 
-        rowCols[0].classList.add(i);
-        rowCols[0].textContent = i;
-      
-        rowCols[1].classList.add(loanObj.monthlyPymt);
-        rowCols[1].textContent = loanObj.monthlyPymt.toLocaleString('en-US', {
+        rowCols[0].textContent = loanArray[index];
+        rowCols[1].textContent = loanArray[index + 1].toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2
         });
 
-        rowCols[2].classList.add(monthlyPrincipal);
-        rowCols[2].textContent = monthlyPrincipal.toLocaleString('en-US', {
+        rowCols[2].textContent = loanArray[index + 2].toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2
         });
 
-        rowCols[3].classList.add(interestPymt);
-        rowCols[3].textContent = interestPymt.toLocaleString('en-US', {
+        rowCols[3].textContent = loanArray[index + 3].toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2
         });
 
-        rowCols[4].classList.add(monthlyInterest);
-        rowCols[4].textContent = monthlyInterest.toLocaleString('en-US', {
+        rowCols[4].textContent = loanArray[index + 4].toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2
         });
 
-        rowCols[5].classList.add(balance);
-        rowCols[5].textContent = balance.toLocaleString('en-US', {
+        rowCols[5].textContent = loanArray[index + 5].toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
             minimumFractionDigits: 2
         });
-        
+
         tableBody.appendChild(tableRow);
-
     }
 }
